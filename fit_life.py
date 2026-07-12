392305c37c05f449161865ffafd45cc74e10f377
Alexey Ignatychev <Alexios94@yandex.ru>
2026-07-12 21:07:59 +0300

    1

--- fit_life.py
+++ fit_life.py
@@ -1,29 +1,17 @@
 # Проект FitLife - MVP версия 1.0
-
-
 # 1. Знакомство
 user_name = input("Пожалуйста введите ваше имя ")
 user_age = int(input("Пожалуйста введите ваш возраст "))
-
-
 # 2. Сбор данных
 user_weight = float(input("Пожалуйста введите ваш вес (66.6кг) "))
 user_height = float(input("Пожалуйста введите ваш рост (1.66м) "))
-
-
-
 # 3. Логика расчетов (Функции как "черный ящик": используем арифметику)
-bmi = (user_weight / (user_height ** 2))    #расчёт ИМТ
-okr_bmi = round(bmi, 1)     #округление ИМТ
-
-
+bmi = (user_weight / (user_height ** 2))   
+okr_bmi = round(bmi, 1)     
 # Подсчет воды: вес * 30 мл
-water_ml = user_weight * 30     # расчёт воды милилитры
-water_l = water_ml / 1000   # перевод воды в литры
-okr_water = round(water_l, 1)   # округление литров
-
-
-
+water_ml = user_weight * 30     
+water_l = water_ml / 1000   
+okr_water = round(water_l, 1)  
 # 4. Вывод красивого результата
 print(f"Здравствуйте {user_name}")
 print(f"Отчёт для пользователя: {user_name}({user_age} лет/года)")
